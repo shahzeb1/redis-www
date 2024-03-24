127dcc6924ec6ad4ea6650f2cb9ea183f8ef3238
Shahzeb Khan <shahzeb001@gmail.com>
2024-03-23 18:31:28 -0700

    Scrollable div

--- src/components/redis-container.tsx
+++ src/components/redis-container.tsx
@@ -18,15 +18,17 @@ const RedisContainer = () => {
   };
 
   return (
-    <div className="bg-slate-800 flex-1 rounded p-4 h-1/4">
-      {!!data.length && <RedisResponses data={data} />}
-      <div className="flex">
-        <Input
-          placeholder="Enter in your Redis command here and press ⏎"
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          onKeyDown={onKeyDown}
-        />
+    <div className="bg-slate-800 flex-1 rounded p-4 h-[calc(100vh-100pt)]">
+      <div className="flex flex-col h-full">
+        <RedisResponses data={data} />
+        <div className="flex">
+          <Input
+            placeholder="Enter in your Redis command here and press ⏎"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            onKeyDown={onKeyDown}
+          />
+        </div>
       </div>
     </div>
   );
