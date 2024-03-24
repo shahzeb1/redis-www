a4dc76e9160b1251fa31a53cf651b1d1555f37f0
Shahzeb Khan <shahzeb001@gmail.com>
2024-03-23 19:25:14 -0700

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import "../app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="application-theme">
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
