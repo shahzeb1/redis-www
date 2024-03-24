347d3be530fe9292aa25cc8c4421cbdf041fb66f
Shahzeb Khan <shahzeb001@gmail.com>
2024-03-23 18:52:50 -0700

    Make things scroll to bottom

--- src/components/redis-container.tsx
+++ src/components/redis-container.tsx
@@ -1,12 +1,13 @@
-import { KeyboardEventHandler, useMemo, useState } from "react";
+import { KeyboardEventHandler, createRef, useMemo, useState } from "react";
 import { Input } from "./ui/input";
 import { Data } from "./types";
 import * as wasmPkg from "../pkg";
-import RedisResponses from "./redis-responses";
+import RedisList from "./redis-list";
 
 const RedisContainer = () => {
   const [input, setInput] = useState("");
   const [data, setData] = useState<Data[]>([]);
+  const ref = createRef<HTMLDivElement>();
   const wasm = useMemo(() => new wasmPkg.WasmRunnerContainer(), []);
 
   const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
@@ -14,13 +15,16 @@ const RedisContainer = () => {
       const output = wasm.run(input);
       setData([...data, { input, output }]);
       setInput("");
+
+      // How we scroll to the bottom of the responses:
+      ref.current?.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
     <div className="bg-slate-800 flex-1 rounded p-4 h-[calc(100vh-100pt)]">
       <div className="flex flex-col h-full">
-        <RedisResponses data={data} />
+        <RedisList data={data} ref={ref} />
         <div className="flex">
           <Input
             placeholder="Enter in your Redis command here and press ⏎"
---
+++ src/components/redis-list.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react";
+import { Data } from "./types";
+
+type Props = {
+  data: Data[];
+};
+
+const RedisResponses = forwardRef<HTMLDivElement, { data: Data[] }>(
+  ({ data }, ref) => {
+    return (
+      <div className="mb-4 max-h-[calc(100vh-100pt)] overflow-y-scroll grow">
+        {data.map((item, i) => (
+          <div key={i} className="my-4 first:mt-0">
+            <div className="text-slate-400">❯ {item.input}</div>
+            <div className="text-slate-100">{item.output}</div>
+          </div>
+        ))}
+        {/* This div is just the point to which we scroll to */}
+        <div ref={ref} className="h-14"></div>
+      </div>
+    );
+  }
+);
+
+export default RedisResponses;
