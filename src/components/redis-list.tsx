574ec4c459f2dbaa42f860639beb4553a5e8f7da
Shahzeb Khan <shahzeb001@gmail.com>
2024-03-23 19:22:17 -0700

    Fix lint issue

--- src/components/redis-list.tsx
+++ src/components/redis-list.tsx
@@ -5,21 +5,19 @@ type Props = {
   data: Data[];
 };
 
-const RedisResponses = forwardRef<HTMLDivElement, { data: Data[] }>(
-  ({ data }, ref) => {
-    return (
-      <div className="mb-4 max-h-[calc(100vh-100pt)] overflow-y-scroll grow no-scrollbar">
-        {data.map((item, i) => (
-          <div key={i} className="my-4 first:mt-0">
-            <div className="text-slate-400">❯ {item.input}</div>
-            <div className="text-slate-100">{item.output}</div>
-          </div>
-        ))}
-        {/* This div is just the point to which we scroll to */}
-        <div ref={ref} className="h-14"></div>
-      </div>
-    );
-  }
-);
+const RedisResponses = forwardRef<HTMLDivElement, Props>(({ data }, ref) => {
+  return (
+    <div className="mb-4 max-h-[calc(100vh-100pt)] overflow-y-scroll grow no-scrollbar">
+      {data.map((item, i) => (
+        <div key={i} className="my-4 first:mt-0">
+          <div className="text-slate-400">❯ {item.input}</div>
+          <div className="text-slate-100">{item.output}</div>
+        </div>
+      ))}
+      {/* This div is just the point to which we scroll to */}
+      <div ref={ref} className="h-14"></div>
+    </div>
+  );
+});
 
 export default RedisResponses;
